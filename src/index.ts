64a8656dc03fbcc23b661d26cae6f67e0e001ed0
Ash Wilson <smashwilson@github.com>
2018-11-15 15:52:23 -0500

    First passing test :tada:

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,109 @@
-import { Application } from 'probot' // eslint-disable-line no-unused-vars
+import { Application, Context } from "probot";
+import metadata from "probot-metadata";
+
+interface IRecordedFailure {
+  readonly path: string;
+  readonly message: string;
+  readonly raw_details: string;
+  readonly details_url: string;
+}
+
+interface IRecordedFailureMap {
+  [sha: string]: IRecordedFailure[];
+}
+
+function editFailures(
+  context: Context,
+  pullNumbers: number[],
+  headSha: string,
+  cb: (before: IRecordedFailure[]) => Promise<void>,
+): Promise<void[]> {
+  const params = context.repo();
+
+  return Promise.all(
+    pullNumbers.map(async (pullNumber) => {
+      const response = await context.github.issues.get({...params, number: pullNumber});
+      const issueish = {
+        ...params,
+        ...response.data,
+      };
+
+      const fullMap: IRecordedFailureMap = (await metadata(context, issueish).get("failures")) || {};
+      let failures = fullMap[headSha];
+      if (!failures) {
+        fullMap[headSha] = failures = [];
+      }
+      await cb(failures);
+      await metadata(context, issueish).set("failures", fullMap);
+    }),
+  );
+}
 
 export = (app: Application) => {
-  app.on('issues.opened', async (context) => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    await context.github.issues.createComment(issueComment)
-  })
-  // For more information on building apps:
-  // https://probot.github.io/docs/
+  app.on("check_run.completed", async (context) => {
+    const run = context.payload.check_run;
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
-}
+    // Extract information from the payload
+
+    const detailsUrl: string = run.details_url;
+    const annotationsCount: number = run.output.annotations_count;
+    const headSha: string = run.head_sha;
+    const pullRequestNumbers: number[] = run.pull_requests.map((each: any) => each.number);
+
+    // Short-circuit and log some common cases we don't care about
+
+    if (pullRequestNumbers.length === 0) {
+      context.log.debug(`ignoring check_run ${run.id} with no linked pull requests`);
+      return;
+    }
+
+    if (annotationsCount === 0) {
+      context.log.debug(`ignoring check_run ${run.id} with no annotations`);
+      return;
+    }
+
+    if (annotationsCount > 5) {
+      context.log.debug(`ignoring check_run ${run.id} with too many (${annotationsCount}) annotations`);
+      return;
+    }
+
+    if (run.conclusion !== "failure" && run.conclusion !== "success") {
+      context.log.debug(`ignoring check_run ${run.id} with conclusion ${run.conclusion}`);
+      return;
+    }
+
+    // Fetch annotation data
+
+    const {data: annotations} = await context.github.checks.listAnnotations(
+      context.repo({check_run_id: run.id}),
+    );
+    const failures = annotations.filter((annotation) => annotation.annotation_level === "failure");
+
+    if (failures.length === 0) {
+      context.log.debug(`ignoring check_run ${run.id} with no failure annotations`);
+      return;
+    }
+
+    // Detect flakes and take appropriate action
+
+    if (run.conclusion === "failure") {
+      context.log.debug(`check_run ${run.id} contains failures`);
+      // Record this failure within the pull request metadata if it is not present already
+      await editFailures(context, pullRequestNumbers, headSha, async (known) => {
+        const knownPaths = new Set(known.map((each) => each.path));
+        for (const annotation of failures) {
+          if (!knownPaths.has(annotation.path)) {
+            known.push({
+              path: annotation.path,
+              message: annotation.message,
+              raw_details: annotation.raw_details,
+              details_url: detailsUrl,
+            });
+          }
+        }
+      });
+    } else if (run.conclusion === "success") {
+      //
+    }
+  });
+};
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,86 +1,92 @@
-import nock from 'nock'
-import dedent from 'dedent'
+import dedent from "dedent";
+import nock from "nock";
 
-import flakeDetector from '../src'
-import { Probot } from 'probot'
+import { Probot } from "probot";
+import flakeDetector from "../src";
 
-nock.disableNetConnect()
+nock.disableNetConnect();
 
-describe('Flake Detector', () => {
-  let probot: any
+describe("Flake Detector", () => {
+  let probot: any;
 
   beforeEach(() => {
-    probot = new Probot({ id: 123, cert: 'test' })
-    const app = probot.load(flakeDetector)
-    app.app = () => 'test'
-  })
+    probot = new Probot({ id: 123, cert: "test" });
+    const app = probot.load(flakeDetector);
+    app.app = () => "test";
 
-  test('stores metadata when a check run reports a failing test', async () => {
+    nock("https://api.github.com")
+      .post("/app/installations/2/access_tokens")
+      .reply(200, { token: "test" });
+  });
+
+  test("stores metadata when a check run reports a failing test", async () => {
     const expectedMetadata = {
-      failures: {
-        deadbeef: [
-          {
-            path: 'test number 1',
-            message: 'message 1',
-            raw_details: 'raw 1',
-            details_url: 'https://ci.that.doesnt.suck/builds/7'
-          },
-          {
-            path: 'test number 2',
-            message: 'message 2',
-            raw_details: 'raw 2',
-            details_url: 'https://ci.that.doesnt.suck/builds/7'
-          }
-        ]
-      }
-    }
+      2: {
+        failures: {
+          deadbeef: [
+            {
+              path: "test number 1",
+              message: "message 1",
+              raw_details: "raw 1",
+              details_url: "https://ci.that.doesnt.suck/builds/7",
+            },
+            {
+              path: "test number 2",
+              message: "message 2",
+              raw_details: "raw 2",
+              details_url: "https://ci.that.doesnt.suck/builds/7",
+            },
+          ],
+        },
+      },
+    };
 
-    const n = nock('https://api.github.com')
-      .get('/repos/owner/repo/check-runs/100/annotations')
-      .reply(200, JSON.stringify([
-        { path: 'test number 1', message: 'message 1', raw_details: 'raw 1', annotation_level: 'failure' },
-        { path: 'test number 2', message: 'message 2', raw_details: 'raw 2', annotation_level: 'failure' },
-        { path: 'idk why this is here', message: 'no', raw_details: 'no', annotation_level: 'warning' }
-      ]))
-      .get('/repos/owner/repo/pulls/6')
-      .reply(200, JSON.stringify({
-        body: 'this is the existing pull request body'
-      }))
-      .patch('/repos/owner/repo/pulls/6', (body: any) => {
-        expect(body).toMatchObject({
-          body: dedent`
-            this is the existing pull request body
+    const n = nock("https://api.github.com")
+      .get("/repos/owner/repo/check-runs/100/annotations")
+      .reply(200, [
+        { path: "test number 1", message: "message 1", raw_details: "raw 1", annotation_level: "failure" },
+        { path: "test number 2", message: "message 2", raw_details: "raw 2", annotation_level: "failure" },
+        { path: "idk why this is here", message: "no", raw_details: "no", annotation_level: "warning" },
+      ])
+      .get("/repos/owner/repo/issues/6")
+      .reply(200, { number: 6, body: "this is the existing pull request body" })
+      .patch("/repos/owner/repo/issues/6", {body: dedent`
+        this is the existing pull request body
 
-            <!-- probot = ${JSON.stringify(expectedMetadata)} -->
-          `
-        })
-      })
-      .reply(200)
+        <!-- probot = ${JSON.stringify(expectedMetadata)} -->
+      `})
+      .reply(200);
 
     await probot.receive({
-      name: 'check_run',
+      name: "check_run",
       payload: {
-        action: 'completed',
+        installation: { id: 2 },
+        repository: { owner: {login: "owner"}, name: "repo" },
+        action: "completed",
         check_run: {
-          name: 'oh no',
-          status: 'completed',
-          conclusion: 'failure',
-          details_url: 'https://ci.that.doesnt.suck/builds/7',
-          output: { annotations_url: 'https://api.github.com/repos/owner/repo/check-runs/100/annotations' },
-          head_sha: 'deadbeef',
-          pull_requests: [ { number: 6 } ]
-        }
-      }
-    })
+          id: 100,
+          name: "oh no",
+          status: "completed",
+          conclusion: "failure",
+          details_url: "https://ci.that.doesnt.suck/builds/7",
+          output: {
+            annotations_count: 2,
+            annotations_url: "https://api.github.com/repos/owner/repo/check-runs/100/annotations",
+          },
+          head_sha: "deadbeef",
+          pull_requests: [ { number: 6 } ],
+        },
+      },
+    });
 
     expect(n.isDone()).toBeTruthy();
-  })
+  });
 
-  test.skip('adds a comment when a check run has identified a potential flake', () => {})
+  test.skip("adds a comment when a check run has identified a potential flake", () => {});
 
-  test.skip('omits a potential flake that already has an issue created', () => {})
+  test.skip("omits a potential flake that already has an issue created", () => {});
 
-  test.skip('creates an issue when a potential flake has been accepted', () => {})
+  test.skip("creates an issue when a potential flake has been accepted", () => {});
 
-  test.skip('declines to create an issue for a potential flake when one already exists', () => {})
-})
+  test.skip("declines to create an issue for a potential flake when one already exists", () => {});
+});
